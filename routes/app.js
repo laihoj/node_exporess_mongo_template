ee730796700037aadd7cf9da762f33469e2b1bc9
Jaakko Laiho <jaakkolaiho94@gmail.com>
2019-12-28 22:42:15 +0200

    development file add

---
+++ routes/app.js
@@ -0,0 +1,27 @@
+const 	express 	= require("express"),
+	 	router 		= express.Router(),
+		passport 	= require("passport");
+
+
+const db = require('./../db.js');
+const utils = require('./../utils.js');
+
+
+
+/*********************************************************************************
+YOUR CODE HERE
+*********************************************************************************/
+// //Example
+// router.get("/application", function(req, res) {
+// 	res.render("pages/index");
+// });
+
+
+
+
+
+/*********************************************************************************
+YOUR CODE OVER
+*********************************************************************************/
+
+module.exports = router;
